a2473ab771ead5618f716f73899192becb8a88c3
Shagufta Naaz Akki <shaguftanaazakki64@gmail.com>
2026-03-23 13:19:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let songs = [
+    {
+        title: "Ilahi",
+        artist: "Arijit Singh",
+        src: "songs/ilahi.mp3",
+        cover: "images/ilahi.jpg"
+    },
+    {
+        title: "Jhol",
+        artist: "Maanu",
+        src: "songs/jhol.mp3",
+        cover: "images/jhol.jpg"
+    },
+    {
+        title: "Parizaad",
+        artist: "Asim Azhar",
+        src: "songs/parizaad.mp3",
+        cover: "images/parizaad.jpg"
+    },
+    {
+        title: "Safarnama",
+        artist: "Lucky Ali",
+        src: "songs/safarnama.mp3",
+        cover: "images/safarnama.jpg"
+    },
+    {
+        title: "Husn",
+        artist: "Anuv Jain",
+        src: "songs/husn.mp3",
+        cover: "images/husn.jpg"
+    },
+    {
+        title: "Sajde",
+        artist: "Arijit Singh",
+        src: "songs/sajde.mp3",
+        cover: "images/sajde.jpg"
+    }
+];
